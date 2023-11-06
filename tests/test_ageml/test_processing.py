5077bf6e99ee4452f6c9f38c2e058084487fe7fb
itellaetxe <inigotellaetxe@gmail.com>
2023-11-06 13:50:39 +0100

    [TST] utils and processing modules, 100% coverage. Added minor parameter control to input arguments.

--- tests/test_ageml/test_processing.py
+++ tests/test_ageml/test_processing.py
@@ -11,3 +11,14 @@ def test_find_correlations():
     expected = np.array([1, 1, -1])
     
     assert np.allclose(result, expected, rtol=1e-10) is True
+
+
+@pytest.mark.parametrize('X, Y, exception_msg', [
+    (np.array([[2, 4, np.nan], [4, 8, -12], [6, 12, -18], [8, 16, -24]]), np.array([1, 2, 3, 4]), "NaN entrie(s) found in X."),
+    (np.array([[2, 4, -6], [4, 8, -12], [6, 12, -18], [8, 16, -24]]), np.array([1, 2, 3, np.nan]), "NaN entrie(s) found in Y.")
+])
+def test_find_correlations_nans(X, Y, exception_msg):
+    with pytest.raises(ValueError) as exc_info:
+        processing.find_correlations(X, Y)
+        assert exc_info.type == ValueError
+    assert str(exc_info.value) == exception_msg
--- tests/test_ageml/test_utils.py
+++ tests/test_ageml/test_utils.py
@@ -4,8 +4,40 @@ import shutil
 import ageml.utils as utils
 
 
-def func_to_log():
-    print('Mondong!')
+class InstanceClass(object):
+    """
+    Dummy class to test the ageml.utils.log decorator.
+    The decorator needs an 'instance' input object, and this
+    input object must have a .log_path attribute, ideally a
+    path.
+    """
+    
+    def __init__(self, log_path):
+        self.log_path = log_path
+
+
+def test_log_decorator():
+    @utils.log
+    def func_to_log(instance, to_print):
+        print(to_print)
+
+    # Define instance and print message
+    dummy_path = os.path.join(os.getcwd(), 'test_log_mondongo')
+    instance_dummy = InstanceClass(dummy_path)
+    to_print = 'Mondongo for the win'
+    
+    # Just in case, remove the log file from possible unsuccessful test run that might have left it undeleted.
+    if os.path.exists(dummy_path):
+        os.remove(dummy_path)
+    func_to_log(instance_dummy, to_print)
+    
+    # Assert that the log exists
+    assert os.path.exists(instance_dummy.log_path)
+    # Assert that the log content is what it is expected
+    with open(instance_dummy.log_path, 'r') as f:
+        assert f.readline() == to_print + "\n"
+    # Cleanup of log file
+    os.remove(dummy_path)
 
 
 @pytest.mark.parametrize("input, expected", [("true", True), ("false", False), ("420.69", 420.69), ("mondongo", "mondongo")])
@@ -22,10 +54,6 @@ def test_create_directory():
     assert os.path.exists(path)
     # Delete after
     shutil.rmtree(path)
-
-
-def test_log_decorator():
-    pass
 
 
 def test_insert_newlines():
