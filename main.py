5054e2861ebf105db6b987d008277064652f6b93
JGarciaCondado <jorgegarciacondado@gmail.com>
2023-11-09 13:52:50 +0100

    Add function to choose between interactive and CLI

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import ageml
+import sys
+
+def main():
+    """Choose between interactive command line or command line 
+    based on wether there are no flags when running script"""
+
+    if len(sys.argv) > 1:
+        ageml.ui.CLI().run()
+    else:
+        ageml.ui.InteractiveCLI().command_interface()
 
 if __name__ == '__main__':
-    ageml.ui.InteractiveCLI().command_interface()
+    main()
