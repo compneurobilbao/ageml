9f74a3f1a3ce1098791431b76a56492415e768d7
itellaetxe <inigotellaetxe@gmail.com>
2024-01-26 12:32:26 +0100

    [ENH] Added printing logistic weights and training a classifier with all systems

--- src/ageml/modelling.py
+++ src/ageml/modelling.py
@@ -518,6 +518,7 @@ class Classifier:
 
         # Arrays to store  values
         accs, aucs, spes, sens = [], [], [], []
+        y = y.ravel()
         y_preds = np.empty(shape=y.shape)
     
         kf = model_selection.KFold(n_splits=self.CV_split, shuffle=True, random_state=self.seed)
