8c348c2291b3e9127356823d58f214cc09edc710
itellaetxe <inigotellaetxe@gmail.com>
2025-09-09 13:41:02 +0200

    ENH: Adds scripts for debugging latest 2 pipelines
    Unrelated to branch, but good adding

---
+++ bin/scripts/debug_age_vs_logistic.py
@@ -0,0 +1,4 @@
+import ageml.commands as commands
+
+if __name__ == "__main__":
+    commands.age_model_vs_logistic_regression()
---
+++ bin/scripts/debug_feature_influence.py
@@ -0,0 +1,4 @@
+import ageml.commands as commands
+
+if __name__ == "__main__":
+    commands.model_feature_influence()
